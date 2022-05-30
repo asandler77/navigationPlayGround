7820be2f8681dfb9227a77b2ec06755b12a62cb2
Sandler, Alexey(as371x) <as371x@intl.att.com>
2022-05-30 19:03:51 +0300

    Add material icons.

--- components/EntryPoint.tsx
+++ components/EntryPoint.tsx
@@ -4,22 +4,54 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './HomeScreen';
 import NotesEntry from './notes/NotesEntry';
 import ProfileSettings from './ProfileSettings';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 const EntryPoint = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator >
+      <Tab.Navigator>
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: 'HomeScreen title'}}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
         />
-        <Tab.Screen name="ProfileSettings" component={ProfileSettings} />
-        <Tab.Screen name="NoteApplication" component={NotesEntry} />
+        <Tab.Screen
+          name="ProfileSettings"
+          component={ProfileSettings}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="account-circle"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="NoteApplication"
+          component={NotesEntry}
+          options={{
+            tabBarLabel: 'Notes',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="account-edit"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
